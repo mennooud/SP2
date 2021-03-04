4cea8f34c45d898f3522d4a5ed1363de4cb55340
LukaHerrmann <lukajherrmann@gmail.com>
2021-03-04 14:53:46 +0100

    Met psycopg2 enkele basale functies gemaakt voor het leggen en breken van een connectie.

--- PGAdmin.py
+++ PGAdmin.py
@@ -1,2 +1,18 @@
 import psycopg2
 
+def makeconnection(host, database, user, password):
+    connection = psycopg2.connect(
+                    host=host,
+                    database=database,
+                    user=user,
+                    password=password)
+    return connection
+
+
+def makecursor(connection):
+    return connection.cursor()
+
+
+def closeconnection(connection, cursor):
+    cursor.close()
+    connection.close()
