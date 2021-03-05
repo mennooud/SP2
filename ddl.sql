4b163b535fcba3f7e66b9860f362aa6dcf6f95ad
LukaHerrmann <lukajherrmann@gmail.com>
2021-03-05 15:54:25 +0100

    Tabel 'Events' weggehaald

--- ddl.sql
+++ ddl.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS Orderedproducts;
 DROP TABLE IF EXISTS Recommendedproducts;
 DROP TABLE IF EXISTS Viewedproducts;
-DROP TABLE IF EXISTS Events;
 DROP TABLE IF EXISTS Products;
 DROP TABLE IF EXISTS Brands;
 DROP TABLE IF EXISTS Categories;
@@ -13,7 +12,6 @@ DROP TABLE IF EXISTS Sub_sub_categories;
 
 CREATE TABLE Brands (brandid SERIAL, brand varchar(255), PRIMARY KEY (brandid));
 CREATE TABLE Categories (categoryid SERIAL, category varchar(255) NOT NULL, PRIMARY KEY (categoryid));
-CREATE TABLE Events (eventid SERIAL, Sessionssessionid varchar(255) NOT NULL, Productsproductid varchar(255), PRIMARY KEY (eventid));
 CREATE TABLE Genders (genderid SERIAL, gender varchar(255), PRIMARY KEY (genderid));
 CREATE TABLE Orderedproducts (orderedproductid SERIAL, Sessionssessionid varchar(255) NOT NULL, Productsproductid varchar(255) NOT NULL, PRIMARY KEY (orderedproductid));
 CREATE TABLE Products (productid varchar(255) NOT NULL, name varchar(255), description varchar(1023), price int, herhaalaankopen BIT, recommendable BIT, Brandsbrandid int, Categoriescategoryid int, Sub_sub_categoriessub_sub_categoryid int, Sub_categoriessub_categoryid int, Gendersgenderid int, PRIMARY KEY (productid));
@@ -31,8 +29,6 @@ ALTER TABLE Products ADD CONSTRAINT FKProducts505288 FOREIGN KEY (Gendersgenderi
 ALTER TABLE Products ADD CONSTRAINT FKProducts973090 FOREIGN KEY (Sub_categoriessub_categoryid) REFERENCES Sub_categories (sub_categoryid);
 ALTER TABLE Products ADD CONSTRAINT FKProducts978174 FOREIGN KEY (Sub_sub_categoriessub_sub_categoryid) REFERENCES Sub_sub_categories (sub_sub_categoryid);
 ALTER TABLE Sessions ADD CONSTRAINT FKSessions493177 FOREIGN KEY (Profilesprofileid) REFERENCES Profiles (profileid);
-ALTER TABLE Events ADD CONSTRAINT FKEvents31703 FOREIGN KEY (Productsproductid) REFERENCES Products (productid);
-ALTER TABLE Events ADD CONSTRAINT FKEvents467990 FOREIGN KEY (Sessionssessionid) REFERENCES Sessions (sessionid);
 ALTER TABLE Orderedproducts ADD CONSTRAINT FKOrderedpro121984 FOREIGN KEY (Productsproductid) REFERENCES Products (productid);
 ALTER TABLE Orderedproducts ADD CONSTRAINT FKOrderedpro685696 FOREIGN KEY (Sessionssessionid) REFERENCES Sessions (sessionid);
 ALTER TABLE Products ADD CONSTRAINT FKProducts897994 FOREIGN KEY (Categoriescategoryid) REFERENCES Categories (categoryid);
