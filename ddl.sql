6540d4d284829b35f91e5487c24b41e8827dbb6c
LukaHerrmann <lukajherrmann@gmail.com>
2021-03-05 11:19:04 +0100

    Een relation tussen 'Profiles' en 'Products' weggehaald, omdat die al door een koppeltabel was verzorgd.

--- ddl.sql
+++ ddl.sql
@@ -16,7 +16,7 @@ CREATE TABLE Categories (categoryid SERIAL, category varchar(255) NOT NULL, PRIM
 CREATE TABLE Events (eventid SERIAL, Sessionssessionid varchar(255) NOT NULL, Productsproductid varchar(255), PRIMARY KEY (eventid));
 CREATE TABLE Genders (genderid SERIAL, gender varchar(255), PRIMARY KEY (genderid));
 CREATE TABLE Orderedproducts (orderedproductid SERIAL, Sessionssessionid varchar(255) NOT NULL, Productsproductid varchar(255) NOT NULL, PRIMARY KEY (orderedproductid));
-CREATE TABLE Products (productid varchar(255) NOT NULL, name varchar(255), description varchar(1023), price float, herhaalaankopen BIT, recommendable BIT, Profilesprofileid varchar(255), Brandsbrandid int, Categoriescategoryid int, Sub_sub_categoriessub_sub_categoryid int, Sub_categoriessub_categoryid int, Gendersgenderid int, PRIMARY KEY (productid));
+CREATE TABLE Products (productid varchar(255) NOT NULL, name varchar(255), description varchar(1023), price float, herhaalaankopen BIT, recommendable BIT, Brandsbrandid int, Categoriescategoryid int, Sub_sub_categoriessub_sub_categoryid int, Sub_categoriessub_categoryid int, Gendersgenderid int, PRIMARY KEY (productid));
 CREATE TABLE Profiles (profileid varchar(255) NOT NULL, PRIMARY KEY (profileid));
 CREATE TABLE Recommendedproducts (recommendedproductid SERIAL, Profilesprofileid varchar(255) NOT NULL, Productsproductid varchar(255) NOT NULL, PRIMARY KEY (recommendedproductid));
 CREATE TABLE Sessions (sessionid varchar(255) NOT NULL, Profilesprofileid varchar(255) NOT NULL, sessionstart timestamp NOT NULL, sessionend timestamp NOT NULL, has_sale BIT NOT NULL, PRIMARY KEY (sessionid));
@@ -37,4 +37,3 @@ ALTER TABLE Orderedproducts ADD CONSTRAINT FKOrderedpro121984 FOREIGN KEY (Produ
 ALTER TABLE Orderedproducts ADD CONSTRAINT FKOrderedpro685696 FOREIGN KEY (Sessionssessionid) REFERENCES Sessions (sessionid);
 ALTER TABLE Products ADD CONSTRAINT FKProducts897994 FOREIGN KEY (Categoriescategoryid) REFERENCES Categories (categoryid);
 ALTER TABLE Products ADD CONSTRAINT FKProducts577926 FOREIGN KEY (Brandsbrandid) REFERENCES Brands (brandid);
-ALTER TABLE Products ADD CONSTRAINT FKProducts634987 FOREIGN KEY (Profilesprofileid) REFERENCES Profiles (profileid);
